ff2154842413634ae1afc7417c3d339bddea6808
AdiUtamaPutra <utamaputra.space21@gmail.com>
2022-02-16 09:21:12 +0800

    Create file method.py

---
+++ 04method.py
@@ -0,0 +1,40 @@
+class COC:
+    #class variable
+    jlm_troops = 0
+
+    def __init__(self,name,hitpoint,damage,armor,level):
+        #instance variable
+        self.name = name
+        self.hitpoint = hitpoint
+        self.damage = damage
+        self.armor = armor
+        self.level = level
+
+        COC.jlm_troops += 1
+
+    #method without return
+    def sayHai(self):
+        print('myname is ' + self.name)
+
+    #method with argument
+    def hitpointUp(self, up):
+        self.hitpoint += up
+
+    #method with return
+    def getHitpoint(self):
+        return self.hitpoint
+
+
+troops1 = COC('Barbarian',45,7,2,1)
+troops2 = COC('Archer',20,7,1,1)
+
+#calling method sayHai()
+troops1.sayHai()
+troops2.sayHai()
+
+#calling method hitpointUp()
+troops1.hitpointUp(10)
+print(troops1.hitpoint)
+
+#calling method getHitpoint()
+print(troops2.getHitpoint())       
